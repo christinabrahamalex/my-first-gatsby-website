e605925f2baa08c7c4bc65e0287d9eeea9c40058
christinabrahamalex <christin.alex@axiomtelecom.com>
2021-07-10 19:10:24 +0400

    Finished part 2 of the gat

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+
+const aboutPage = () => {
+    return(
+        <Layout pageTitle="About Me">
+            <p>About meh yohohoho</p>
+        </Layout>
+    )
+};
+
+export default aboutPage
