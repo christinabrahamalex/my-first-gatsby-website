b6032b4012c1f32b3ea3501abd9aeeb2829d293a
christinabrahamalex <christin.alex@axiomtelecom.com>
2021-07-10 19:31:54 +0400

    One piece imgs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import * as React from "react";
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image'
 
 const indexPage = () => {
   return(
     <Layout pageTitle="Home page">
       <p>Will be a great gatsby</p>
+      <StaticImage src="https://i.pinimg.com/originals/55/6d/99/556d99dec040634f46a27f25159da3a8.jpg"/>
+      <StaticImage src="../images/eyepops.jpg"/>
     </Layout>
   )
 };
